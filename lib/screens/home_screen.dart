9d3a80b9570269e0242c47c7f64835f8fb29e395
Max on Flutter <42350837+maxonflutter@users.noreply.github.com>
2023-10-28 22:39:22 +0400

    App bar for the Home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:airbnb_clone/shared/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../widgets/property_type_list.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,7 +13,95 @@ class HomeScreen extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
 
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        toolbarHeight: 128,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            color: appWhite,
+            boxShadow: [
+              BoxShadow(
+                color: appBlack.withOpacity(0.1),
+                blurRadius: 1.0,
+                spreadRadius: 1.0,
+                offset: const Offset(0.0, 1.0),
+              )
+            ],
+          ),
+          child: Stack(
+            children: [
+              const Positioned(
+                bottom: 0.0,
+                left: 0.0,
+                right: 0.0,
+                child: PropertyTypeList(),
+              ),
+              Positioned(
+                top: 70.0,
+                right: 8.0,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.tune),
+                ),
+              ),
+              Positioned(
+                left: 16.0,
+                right: 72.0,
+                top: 64.0,
+                child: GestureDetector(
+                  onTap: () {
+                    context.pushNamed('booking-details');
+                  },
+                  child: Hero(
+                    tag: 'search',
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0,
+                        vertical: 8.0,
+                      ),
+                      decoration: BoxDecoration(
+                        color: appWhite,
+                        border: Border.all(
+                          color: appGrey,
+                          width: 3.0,
+                        ),
+                        borderRadius: BorderRadius.circular(32.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: appGrey.withOpacity(0.5),
+                            blurRadius: 8.0,
+                            spreadRadius: 8.0,
+                            offset: const Offset(0.0, 4.0),
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.search),
+                          const SizedBox(width: 8.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Where to?',
+                                style: textTheme.bodyMedium!
+                                    .copyWith(fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                'Anywhere • Any week • Add guest',
+                                style: textTheme.bodyMedium,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Container(),
     );
   }
--- lib/widgets/property_type_list.dart
+++ lib/widgets/property_type_list.dart
@@ -1,1 +1,67 @@
+import 'package:flutter/material.dart';
 
+import '../shared/theme/colors.dart';
+
+class PropertyTypeList extends StatefulWidget {
+  const PropertyTypeList({super.key});
+
+  @override
+  State<PropertyTypeList> createState() => _PropertyTypeListState();
+}
+
+class _PropertyTypeListState extends State<PropertyTypeList> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    final textTheme = Theme.of(context).textTheme;
+
+    final List<Map<String, dynamic>> propertyTypes = [
+      {'type': 'Hotel', 'icon': Icons.hotel},
+      {'type': 'Apartment', 'icon': Icons.apartment},
+      {'type': 'BnB', 'icon': Icons.bedroom_child},
+      {'type': 'Villa', 'icon': Icons.villa},
+      {'type': 'Resort', 'icon': Icons.holiday_village},
+    ];
+    return SizedBox(
+      height: 56.0,
+      child: ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: propertyTypes.length,
+        itemBuilder: (context, index) {
+          return Container(
+            width: size.width * 0.2,
+            margin: const EdgeInsets.only(
+              right: 4.0,
+              left: 4.0,
+              top: 4.0,
+            ),
+            child: Column(
+              children: [
+                Icon(propertyTypes[index]['icon']),
+                const SizedBox(height: 2.0),
+                Text(
+                  propertyTypes[index]['type'],
+                  style: textTheme.bodySmall!.copyWith(
+                    fontWeight: (index == selectedIndex)
+                        ? FontWeight.bold
+                        : FontWeight.normal,
+                  ),
+                ),
+                (index == selectedIndex)
+                    ? Container(
+                        margin: const EdgeInsets.only(top: 8.0),
+                        height: 2.0,
+                        width: 64.0,
+                        color: appBlack,
+                      )
+                    : const SizedBox()
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
